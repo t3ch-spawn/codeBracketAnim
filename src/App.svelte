55024e432e68d1c56243934696dcf08512400a14
Tife <boluwatife1904@gmail.com>
2024-01-10 20:29:18 +0100

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import lottie from "lottie-web";
+  import logo from "./assets/final2.json";
+  import { onMount } from "svelte";
+
+  let logoAnim;
+
+  onMount(() => {
+    // LOTTIE ANIMATION
+    logoAnim = lottie.loadAnimation({
+      container: document.querySelector(".logo-animation"),
+      animationData: logo,
+      loop: true,
+      autoplay: true,
+    });
+  });
+</script>
+
+<main>
+  <div class="logo-animation"></div>
+</main>
+
+<style>
+</style>
